b602812f472af6650ccec0122a9d09585883a91a
Zubaer Alam <zobaeralam@Iut-dhaka.edu>
2022-08-11 16:27:22 +0600

    added auth middleware for logout

---
+++ SDN/middleware/auth.middleware.js
@@ -0,0 +1,8 @@
+const ensureAuth = (req, res, next) => {
+   if ((req, isAuthenticated())) {
+      next()
+   } else {
+      res.redirect("/users/login")
+   }
+}
+module.exports = ensureAuth
--- SDN/routes/users.routes.js
+++ SDN/routes/users.routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-
+const ensureAuth = require("./../middleware/auth.middleware")
 const {
    getLogin,
    postLogin,
@@ -13,5 +13,10 @@ router.post("/login", postLogin)
 
 router.get("/register", getRegister)
 router.post("/register", postRegister)
+
+router.get("/logout", (req, res) => {
+   req.logOut()
+   res.redirect("/")
+})
 
 module.exports = router
