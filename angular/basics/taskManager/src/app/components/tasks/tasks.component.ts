ca1d60dff439189d36df1bdab724697b71d5a031
Zubaer Alam <zobaeralam@Iut-dhaka.edu>
2022-08-23 06:05:15 +0600

    turned tasks into and observable

--- angular/basics/taskManager/src/app/components/tasks/tasks.component.ts
+++ angular/basics/taskManager/src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
-import { TASKS } from 'src/app/mockTasks';
-import { Task } from 'src/app/Task';
 import { Component, OnInit } from '@angular/core';
-
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   // TASK is a const imported from mockTasks.ts (DUmmy JSON server)
   // Task[] is the type of tasks which is imported from Task.ts
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
